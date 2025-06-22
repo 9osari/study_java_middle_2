8b753931dac10b3bf65b2ff5b8549da6f000ca98
9osari <128437395+9osari@users.noreply.github.com>
2025-06-22 14:10:38 +0900

    docs: 문제와 풀이1 제네릭 메서드와 상한

--- src/generic/ex5/EraserBox.java
+++ src/generic/ex5/EraserBox.java
@@ -5,11 +5,12 @@ public class EraserBox<T> {
     //컴파일 이후에는 제네릭 타입 정보가 존재하지 않는다.
     //따라서 런타임에 타입을 화용하는 코드는 작성불가
     public boolean instanceCheck(Object param) {
-        return param instanceof T; //오류
+        //return param instanceof T; //오류
+        return true;
     }
 
     public T create() {
-        return new T(); //오류
+        //return new T(); //오류
         return null;
     }
 }
