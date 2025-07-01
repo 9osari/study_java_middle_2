535a7cf66ed2179b6b834322cc856687b3c6f302
9osari <128437395+9osari@users.noreply.github.com>
2025-07-01 21:39:54 +0900

    docs: 노드와 연결2

--- src/collection/link/Node.java
+++ src/collection/link/Node.java
@@ -7,4 +7,21 @@ public class Node {
     public Node(Object item) {
         this.item = item;
     }
+
+    //[A->B->C]
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        Node x = this;
+        sb.append("[");
+        while (x != null) {
+            sb.append(x.item);
+            if (x.next != null) {
+                sb.append("->");
+            }
+            x = x.next;
+        }
+        sb.append("]");
+        return sb.toString();
+    }
 }
